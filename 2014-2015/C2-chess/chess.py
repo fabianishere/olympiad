8d2fd1b0724e37200ad74825d9b9e7b9263a02b7
Fabian M <mail.fabianm@gmail.com>
2014-10-20 18:36:15 +0200

    Update solution for problem C2

--- 2014-2015/C2-chess/chess.py
+++ 2014-2015/C2-chess/chess.py
@@ -14,72 +14,84 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import copy, time
+import time
 
-def traverse(matrix, size, point, step, steps = []):
+def traverse(matrix, size, point, step, path = []):
 	x, y = point
-	xb, yb = size
+	w, l = size
 	xs, ys = step
 	res = []
 
-	matrix = copy.deepcopy(matrix)
-	matrix[y][x] += 1
+	# Clone path. Faster than copy.copy()
+	path = path[:]
+	path.append(point)
 
-	steps = copy.deepcopy(steps)
-	steps.append(point)
+	# Clone matrix. Faster than copy.deepcopy()
+	matrix = [row[:] for row in matrix]
+	matrix[y][x] = len(path)
 
-	print("point={0}, steps={1}".format(point, len(steps)))
-	for row in matrix:
-		print(row)
-	time.sleep(0.05)
-
+	# Debugging
+	#print("point={0}, steps={1}".format(point, len(path)))
+	#for row in matrix:
+	#	print(row)
+	#time.sleep(0.5)
+	
 	# Get all unvisited points in the matrix.
 	test = [x for y in matrix for x in filter(lambda x: x == 0, y)]
 	if not len(test):
 		# Return if all points in the matrix have been visited.
-		return [(True, steps)]
+		return [(0, path)]
 	# All path we can take, ignoring the boundaries of the matrix
 	#	and the values of the points.
 	points = [
 		(x - xs, y - ys),
-		(x + xs, y - ys),
-		(x - xs, y + ys),
-		(x + xs, y + ys),
 		(x - ys, y - xs),
+
+		(x + xs, y - ys),
 		(x + ys, y - xs),
+
+		(x - xs, y + ys),
 		(x - ys, y + xs),
-		(x + ys, y + xs)
+		
+		(x + xs, y + ys),
+		(x + ys, y + xs)	
 	]
 	# Remove points that lie beyond the boundaries of the matrix.
-	points = filter(lambda point: 0 <= point[0] < xb and 0 <= point[1] < yb, points)
+	points = filter(lambda point: 0 <= point[0] < w and 0 <= point[1] < l, points)
 	# Remove points that have already been visited.
 	points = filter(lambda point: matrix[point[1]][point[0]] == 0, points)
 	points = list(points)
 	# Check if there are still points available to visit.
 	if not points:
-		# Yep, we're fucked now.
-		# You'll enter a infinite loop now.
-		return [(False, steps)]
+		# Yep, you're fucked now.
+		return [(len([x for y in matrix for x in filter(lambda x: x == 0, y)]), path)]
 	# Get the distance from the points to the boundaries.
-	db = map(lambda point: min([point[0], xb - point[0]]) + min([point[1], yb - point[1]]), points)
+	db = map(lambda point: min(point[0], w - point[0] - 1) + min(point[1], l - point[1] - 1), points)
 	db = list(db)
 	# Shortest distance from a point to the boundaries of the matrix found.
 	minimum = min(db)
-	# Try all points that share the minimum.
-	while minimum in db:
-		index = db.index(minimum)
-		best = points[index]
-		res.extend(traverse(matrix, size, best, step, steps))
-		db.pop(index)
-	return res 
+	# The points which have the shortest distance to the edges.
+	points = [points[i] for i, m in enumerate(db) if m == minimum] 
+	points = map(lambda point: (min(min(point[0], w - point[0] - 1), min(point[1], l - point[1] - 1)), point), points)
+	points = sorted(points, key=lambda k: k[0])
+	pl = list(points)
+	points = filter(lambda x: x[0] == pl[0][0], points)
+	points = map(lambda x: x[1], points)
+
+	# Visit the best points available.
+	for best in points:
+		res.extend(traverse(matrix, size, best, step, path))
+		
+	return res
 
 if __name__ == "__main__":
-	size = list(map(lambda x: int(x), input().split()))
-	step = list(map(lambda x: int(x), input().split()))
+	size, step = [list(map(lambda x: int(x), input().split())) for _ in range(2)]
 	w, h = size
 	matrix = [[0] * w for _ in range(h)]
-	for row in matrix:
-		print(row)
-	print(traverse(matrix, size, (0, 0), step))
-	
-	
+	results = traverse(matrix, size, (0, 0), step)
+	results = sorted(results, key=lambda k: k[0])
+	if len(results) > 0:
+		unvisited, path = results[0]
+		print(len(path))
+		for x, y in path:
+			print("{0}{1}".format(chr(x + ord('a')), h - y))
