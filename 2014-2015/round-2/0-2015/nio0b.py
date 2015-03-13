77a306449d1702beff983c85a9d986fd7c3b9084
Fabian M <mail.fabianm@gmail.com>
2015-03-13 20:12:57 +0100

    Improve algorithm

--- 2014-2015/round-2/0-2015/nio0b.py
+++ 2014-2015/round-2/0-2015/nio0b.py
@@ -3,9 +3,6 @@ while True:
     e = int(input())
     m.append(e)
     if e in m[:-1]: break
-
-last = m[-1]
-start = m.index(last)
-print(m)
+start = m.index(m[-1])
 print(start)
 print(len(m) - start - 1)
--- 2014-2015/round-2/0-2015/nio0d.py
+++ 2014-2015/round-2/0-2015/nio0d.py
@@ -4,14 +4,18 @@ while True:
     e = int(input())
     m.append(e)
     if e in m[:-1]: break
-def rec_sum(iterable, initial=0):
+def sumr(iterable, initial=0):
     while True:
-        initial += next(iterable)
-        yield initial
+        try:
+            initial += next(iterable)
+            yield initial
+        except StopIteration:
+            return
 start = m[:m.index(m[-1])]
 loop = m[m.index(m[-1]):-1]
-if sum(loop) % 2015 == 0:
+partial = sumr(itertools.cycle(loop), sum(start))
+test = lambda x: x % 2015 == 0
+if sum(loop) % 2015 == 0 and not list(filter(test, sumr(iter(loop), sum(start)))):
     print(0)
 else:
-    print(next(filter(lambda x: x % 2015 == 0, rec_sum(itertools.cycle(loop), sum(start)))))
-
+    print(len(start) + next(filter(lambda t: test(t[1]), enumerate(partial)))[0] + 1)
